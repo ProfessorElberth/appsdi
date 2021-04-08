94473640643a439be59e670e855a5ecc9c1a8988
Elberth Moraes <elberth.moraes@prof.infnet.edu.br>
2021-04-08 08:00:31 -0300

    Atualizacao da classe de arquivo

--- src/main/java/br/edu/infnet/appsdi/tp4/FuncionarioArquivoTeste.java
+++ src/main/java/br/edu/infnet/appsdi/tp4/FuncionarioArquivoTeste.java
@@ -5,20 +5,52 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class FuncionarioArquivoTeste {
 	
+	private static Scanner in;
+	private static String opcao;	
 	private static Map<Integer, Funcionario> mapaFuncionario;
+	private static Integer id;
 
 	public static void main(String[] args) {		
+		in = new Scanner(System.in);
+		opcao = null;
+
 		init("C:/dev/", "funcionarios.txt");
 		
-		//TODO chamar o comportamente existente na tela de FuncionarioLoteTeste
+		do {
+			
+			int opcaoMenu = recuperarOpcaoMenu();
+			
+			switch (opcaoMenu) {
+			case 1:
+				incluirProgramador();
+				break;
 
-		//TODO mover este conteúdo para o método obterLista
-		for(Funcionario f: mapaFuncionario.values()) {
-			f.impressao();
-		}		
+			case 2:
+				incluirAnalista();
+				break;
+
+			case 3:
+				obterLista();
+				break;
+
+			case 9:
+				sair();
+				break;
+				
+			default:
+				tratarOpcaoInvalida();
+				break;
+			}
+						
+		} while ("S".equalsIgnoreCase(opcao));
+
+		System.out.println("Processamento finalizado!!!");
+
+		in.close();		
 	}
 
 	private static void incluir(Funcionario funcionario) {
@@ -36,7 +68,7 @@ public class FuncionarioArquivoTeste {
 			
 			mapaFuncionario = new HashMap<Integer, Funcionario>();
 			
-			Integer id = 1;
+			id = 1;
 			
 			while(linha != null) {
 				
@@ -85,4 +117,104 @@ public class FuncionarioArquivoTeste {
 			System.out.println("Problemas no arquivo: " + e.getMessage());
 		}				
 	}
+
+	private static int recuperarOpcaoMenu() {
+		System.out.println("Cadastramento de funcionários:");
+		System.out.println("1 - Incluir programadores");
+		System.out.println("2 - Incluir analistas");
+		System.out.println("3 - Exibir todos");
+		System.out.println("9 - Sair");
+		System.out.print("Escolha a sua opção: ");
+
+		return in.nextInt();
+	}
+
+	private static void obterLista() {
+		
+		if(mapaFuncionario.size() > 0) {
+			System.out.println("Listagem geral de funcionários:");
+			for(Funcionario f : mapaFuncionario.values()) {
+				
+				String tipo = f instanceof Programador ? "P" : "A";
+				
+				System.out.print(tipo + " - ");
+				
+				f.impressao();
+			}
+		} else {
+			System.out.println("Nenhum funcionário cadastrado!!!");
+		}
+		opcao = "S";
+	}
+	
+	private static void sair() {
+		opcao = "N";
+	}
+	
+	private static void tratarOpcaoInvalida() {
+		System.out.println("Opção inválida!!! Informe uma nova opção!");
+		opcao = "S";
+	}
+
+	private static void incluirAnalista() {
+		AnalistaSistema a = new AnalistaSistema();
+		
+		a.setId(id++);
+		
+		System.out.print("Informe o seu nome: ");
+		a.setNome(in.next());
+		
+		System.out.print("Informe o seu e-mail: ");
+		a.setEmail(in.next());
+		
+		System.out.print("Informe o seu salario: ");
+		a.setSalario(in.nextFloat());
+
+		System.out.print("Informe a sua idade: ");
+		a.setIdade(in.nextInt());
+		
+		System.out.print("Informe a quantidade de sistemas: ");
+		a.setQtdeSistema(in.nextInt());
+		
+		System.out.print("Informe o tempo de experiência: ");
+		a.setTempoExperiencia(in.nextInt());
+		
+		a.impressao();
+		
+		System.out.print("Deseja continuar no sistema (S/N)? Resposta: ");
+		opcao = in.next();
+
+		incluir(a);
+	}
+
+	private static void incluirProgramador(){
+		Programador p = new Programador();
+		
+		p.setId(id++);
+		
+		System.out.print("Informe o seu nome: ");
+		p.setNome(in.next());
+		
+		System.out.print("Informe o seu e-mail: ");
+		p.setEmail(in.next());
+		
+		System.out.print("Informe o seu salario: ");
+		p.setSalario(in.nextFloat());
+
+		System.out.print("Informe a sua idade: ");
+		p.setIdade(in.nextInt());
+		
+		System.out.print("Informe a sua linguagem: ");
+		p.setLinguagem(in.next());
+		
+		System.out.print("Informe o seu valor adicional: ");
+		p.setValorAdicional(in.nextFloat());
+
+		p.impressao();
+		
+		System.out.print("Deseja continuar no sistema (S/N)? Resposta: ");
+		opcao = in.next();
+
+		incluir(p);
+	}	
 }
